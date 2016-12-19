db52c8df8397fa9a53865a3c7cea1e77544c940e
Rodrigo Merida <jrodrigomg@hotmail.com>
2016-12-18 20:32:51 -0600

    Servidor

---
+++ server.js
@@ -0,0 +1,21 @@
+var restify = require('restify');
+var plugins = require('restify-plugins');
+ 
+ 
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+server.use(plugins.acceptParser(server.acceptable));
+server.use(plugins.queryParser());
+server.use(plugins.bodyParser());
+ 
+ 
+server.get('/echo/:name', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+ 
+server.listen(8124, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
